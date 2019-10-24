a2b076bcc7bc71914954186622b5191d6be84871
yourUsername <yourEmail@domain.com>
2019-10-23 20:58:14 -0400

    mt

---
+++ src/mathstwo.java
@@ -0,0 +1,19 @@
+import javax.lang.model.element.VariableElement;
+import java.lang.invoke.VarHandle;
+
+
+public class mathstwo {
+
+public static void main (String[]args){
+int mynumber = 2;
+  double myothernumber = 1.7938;
+
+int one; double two;
+System.out.println("mynumber");
+System.out.println("myothernumber");
+
+}
+
+
+
+}
